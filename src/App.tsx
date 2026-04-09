ee6f440e0b95f5e71d26fc251ed28e9de950c7b0
FAN, Sixing <dynamics986@gmail.com>
2026-04-09 15:03:08 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/cv" element={<CV />} />
-        <Route path="*" element={<Navigate to="/\" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Layout>
   );
